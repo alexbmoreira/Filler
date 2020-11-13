643131490ce55c8d871af28b3eac7aaa87e649c5
alexbmoreira <alexbmoreira@gmail.com>
2020-11-12 23:59:11 -0500

    Created game class

---
+++ filler/game.py
@@ -0,0 +1,14 @@
+import color
+import random
+
+class Game():
+    colors = [
+        Color("red", 0),
+        Color("blue", 0),
+        Color("green", 0),
+        Color("purple", 0),
+        Color("yellow", 0),
+        Color("black", 0)
+    ]
+
+    board = []
