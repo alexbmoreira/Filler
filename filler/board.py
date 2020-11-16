ef1a7025217ae8c25cdbb56016a7a037982687e9
alexbmoreira <alexbmoreira@gmail.com>
2020-11-16 16:42:07 -0500

    Changed Game class __str__ function to work for Board class

--- filler/board.py
+++ filler/board.py
@@ -1,7 +1,17 @@
 class Board():
     
-    cols = []
-    rows = [cols]
+    grid = []
 
     def __init__(self, size):
         self.size = size
+
+    def __str__(self):
+        return_string = ""
+
+        for i in range(self.size):
+            for j in range(self.size):
+                return_string += f"{self.grid[i][j].color}"
+                return_string += " " * (self.size - len(self.grid[i][j].color))
+            return_string += "\n"
+
+        return return_string
