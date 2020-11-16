85a78b1d7cc47cf92f0a69cb2fe2a2207707130e
alexbmoreira <alexbmoreira@gmail.com>
2020-11-16 16:56:58 -0500

    Move createboard function from Game class to Board class

--- filler/board.py
+++ filler/board.py
@@ -1,9 +1,21 @@
+import color
+import random
+
 class Board():
-    
+    colors = [
+        color.Color("red", 0),
+        color.Color("blue", 0),
+        color.Color("green", 0),
+        color.Color("purple", 0),
+        color.Color("yellow", 0),
+        color.Color("black", 0)
+    ]
+
     grid = []
 
     def __init__(self, size):
         self.size = size
+        self.createBoard()
 
     def __str__(self):
         return_string = ""
@@ -15,3 +27,18 @@ class Board():
             return_string += "\n"
 
         return return_string
+
+    def createBoard(self):
+        above_color = ""
+        for i in range(self.size):
+            prev_color = ""
+            row = []
+            for j in range(self.size):
+                above_color = self.grid[i - 1][j].color if i > 0 else ""
+                valid_colors = [col for col in self.colors if col.color != prev_color and col.color != above_color]
+
+                add_color = valid_colors[random.randint(0, len(valid_colors) - 1)]
+                prev_color = add_color.color
+
+                row.append(add_color)
+            self.grid.append(row)
--- filler/game.py
+++ filler/game.py
@@ -1,38 +1,13 @@
-import color
 import board as brd
-import random
 
 class Game():
-    colors = [
-        color.Color("red", 0),
-        color.Color("blue", 0),
-        color.Color("green", 0),
-        color.Color("purple", 0),
-        color.Color("yellow", 0),
-        color.Color("black", 0)
-    ]
 
     board = brd.Board(8)
-
-    def createBoard(self):
-        above_color = ""
-        for i in range(self.board.size):
-            prev_color = ""
-            row = []
-            for j in range(self.board.size):
-                above_color = self.board.grid[i - 1][j].color if i > 0 else ""
-                valid_colors = [col for col in self.colors if col.color != prev_color and col.color != above_color]
-
-                add_color = valid_colors[random.randint(0, len(valid_colors) - 1)]
-                prev_color = add_color.color
-
-                row.append(add_color)
-            self.board.grid.append(row)
 
     def __str__(self):
         return str(self.board)
 
 if __name__ == "__main__":
     game = Game()
-    game.createBoard()
+
     print(str(game))
