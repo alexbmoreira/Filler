66b6b0ac710ceec0c583bd047e94ee2defb1763e
alexbmoreira <alexbmoreira@gmail.com>
2020-11-16 19:53:47 -0500

    Flask app now finds static files in filler/static directory

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, make_response, render_template, jsonify, request, session, redirect, url_for
 
-app = Flask(__name__, template_folder='filler/templates')
+app = Flask(__name__, template_folder='filler/templates', static_folder='filler/static')
 
 @app.route('/')
 def index():
