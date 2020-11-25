8633429a65adf9c86a6ee70444363567a1825a28
alexbmoreira <alexbmoreira@gmail.com>
2020-11-25 12:09:21 -0500

    Added dummy function to check that test work

--- filler/tests/test_models.py
+++ filler/tests/test_models.py
@@ -5,18 +5,7 @@ from ..filler.models import Color
 class TestColor(unittest.TestCase):
 
     def test_toJSON(self):
-        self.assertEqual('foo'.upper(), 'FOO')
-
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
-
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
+        self.assertEqual(Color("Red").toJSON(), "Red")
 
 if __name__ == '__main__':
     unittest.main()
