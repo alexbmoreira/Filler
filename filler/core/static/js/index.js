879c474ecfcd94c5593335256f42088c97585f1a
alexbmoreira <alexbmoreira@gmail.com>
2020-11-27 10:54:55 -0500

    Computer makes moves

--- filler/core/static/js/index.js
+++ filler/core/static/js/index.js
@@ -1,18 +1,41 @@
 $(document).ready(function()
 {
+    can_move = true;
     $(document).on("click", ".option-tile.valid", function()
     {
-        console.log("hello")
         color = $(this).data("color");
-        $.ajax({
-            type: "POST",
-            data: { color: color },
-            url: "/make-move",
-            success: function(response)
+        if(can_move)
+        {
+            $.ajax(
             {
-                $("#game-container").html(response);
-                console.log(response);
-            }
-        });
+                type: "POST",
+                data: { color: color },
+                url: "/make-move",
+                success: async function(response)
+                {
+                    $("#game-container").html(response);
+                    can_move = false;
+
+                    await delay(2000);
+                    aiMakeMove();
+                }
+            });
+        }
     });
 });
+
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
+function aiMakeMove()
+{
+    $.ajax(
+    {
+        type: "GET",
+        url: "/ai-make-move",
+        success: async function(response)
+        {
+            $("#game-container").html(response);
+            can_move = true;
+        }
+    });
+}
